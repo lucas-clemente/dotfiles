47f33e0acec6de2c689f21871b391c5f19944a69
Lucas Clemente <luke.clemente@gmail.com>
2013-04-09 17:23:15 +0200

    added rbenv to path

--- .zsh/paths.zsh
+++ .zsh/paths.zsh
@@ -6,4 +6,5 @@ export PATH=/usr/local/share/npm/bin:$PATH
 export PATH=/usr/local/share/python:$PATH
 export PATH=/usr/local/texlive/2012/bin/x86_64-darwin:$PATH
 export PATH=/usr/local/heroku/bin:$PATH
+export PATH=~/.rbenv/bin:$PATH
 export PATH=~/bin:$PATH
