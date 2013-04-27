189772f03f982d50ff8e47f346e25ee9be1f12fb
Lucas Clemente <luke.clemente@gmail.com>
2013-04-27 13:46:54 +0200

    fix history search

--- .zsh/20-history-search.zsh
+++ .zsh/20-history-search.zsh
@@ -1,1 +1,7 @@
 source ~/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh
+# bind UP and DOWN arrow keys
+for keycode in '[' '0'; do
+  bindkey "^[${keycode}A" history-substring-search-up
+  bindkey "^[${keycode}B" history-substring-search-down
+done
+unset keycode
